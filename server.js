f57f868a1f9d96403aea9c2e29095769b9e2f760
AldinBe <aldinbecic1996@gmail.com>
2018-08-08 22:52:24 +0200

    a

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8000
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.use(express.static(__dirname + '/material'));
-
-app.get('/', (req, res) => res.sendStatus(200));
-app.listen(port,function(){
-  console.log('Node app is running on port', port);
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/home'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
