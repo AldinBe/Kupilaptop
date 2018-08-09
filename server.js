8ed8a788aa6dba0c0e59d7872937c58e713614f6
AldinBe <aldinbecic1996@gmail.com>
2018-08-09 11:27:53 +0200

    a

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require('express');
-var app = express();
+var app = express()
 var port = process.env.PORT || 5000
 
-app.use(express.static(__dirname + '/static'));
+var mongojs = require('mongojs')
+var db = mongojs('localhost:27017/kupilaptopDB', ['laptopi'])
 
-app.get('/', (req, res) => res.sendStatus(200));
-app.listen(port,function(){
-  console.log('Node app is running on port', port);
-});
+var body_parser = require('body-parser')
+app.use(body_parser.json())
+
+app.use(express.static(__dirname + '/static'))
+
+app.get('/laptopi', function(req, res){
+  db.laptopi.find(function(err,docs){
+    res.json(docs)
+  })
+})
+
+app.listen(port, function(){
+  console.log('Node app is running on port', port)
+})
